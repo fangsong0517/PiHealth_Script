6560c5e7ae43756e282664c76d5f1e09f009097a
fangsong0517 <2035756541@qq.com>
2020-04-25 23:57:03 +0800

    CPU信息获取

--- 1.Cpu.sh
+++ 1.Cpu.sh
@@ -28,3 +28,41 @@ elif [[ `echo "${CpuTemp} >= 50" | bc -l` == 1 ]];then
 fi
 
 echo "$Time $LoadAvg $CpuUsedPerc% ${CpuTemp}℃  ${WarnLevel}"
+
+
+
+
+
+
+
+
+
+#nowtime_load=`date +"%Y-%m-%d__%H:%M:%S "``uptime | tr -s " " "\n" | tr -s "," " "|awk NF | tail -3 | tr -s "\n" " "`
+#开机后总时间
+#cpu_local1=`cat /proc/stat | grep -w 'cpu'|awk '{printf("%d", $2+$3+$4+$5+$6+$7+$8);}'`
+#开机后累计休闲
+#cpu_idle1=`cat /proc/stat | grep -w 'cpu' | awk '{printf("%d", $5);}'`
+
+#sleep 0.5s
+
+#开机后总时间
+#cpu_local2=`cat /proc/stat | grep -w 'cpu'|awk '{printf("%d", $2+$3+$4+$5+$6+$7+$8);}'`
+#开机后累计休闲
+#cpu_idle2=`cat /proc/stat | grep -w 'cpu' | awk '{printf("%d", $5);}'`
+
+#(( cpu_local=$cpu_local2-$cpu_local1 ))
+#(( cpu_idle=$cpu_idle2-$cpu_idle1 ))
+#cpu占用率
+#cpu_usage=`echo "$cpu_local $cpu_idle" | awk '{printf("%.2f%%",(1 - $2 / $1)*100);}'`
+#cpu温度
+#cpu_temp=`cat /sys/class/thermal/thermal_zone0/temp | awk '{
+#if($1 >= 1000 && $1 < 50000) {
+#    printf("%.2f note", $1 / 1000);
+#} else if($1 >= 50000 && $1 <= 70000) {
+#    printf("%.2f note", $1 /1000);
+#} else {
+#    printf("%.2f warning", $1 / 1000);
+#}
+#}'`
+#echo "$nowtime_load $cpu_usage $cpu_temp"
+
